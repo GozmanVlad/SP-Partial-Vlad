c96edcff5533a57af774032ab5fef64fdae43ae6
Gozman Vlad <vlad.gozman02@e-uvt.ro>
2023-11-21 08:22:55 +0200

    Fist commit

---
+++ src/main/java/ro/uvt/info/partiallab2023/PartialLab2023Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.partiallab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartialLab2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartialLab2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/ro/uvt/info/partiallab2023/PartialLab2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.partiallab2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartialLab2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
